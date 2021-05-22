8873497ffb9da00670b15b9a74cde8b46a9ccb9f
sergey_ivanenko <storm06rk@gmail.com>
2021-05-22 18:50:37 +0300

    feat: subtask1 has been done

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,29 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return makeHappyArray(sadArray);
+    }
+
+    private fun makeHappyArray(sadArray: IntArray): IntArray {
+        val happyArray:ArrayList<Int> = ArrayList();
+        var countSadElementsInArray = 0;
+
+        for (i in sadArray.indices) {
+            if(i == 0 || i == sadArray.size - 1) {
+                happyArray.add(sadArray[i]);
+                continue;
+            }
+            if (sadArray[i - 1] + sadArray[i + 1] >= sadArray[i]) {
+                happyArray.add(sadArray[i]);
+                continue;
+            }
+            countSadElementsInArray++;
+        }
+
+        return if (countSadElementsInArray == 0) {
+            happyArray.toIntArray();
+        } else {
+            makeHappyArray(happyArray.toIntArray());
+        }
     }
 }
