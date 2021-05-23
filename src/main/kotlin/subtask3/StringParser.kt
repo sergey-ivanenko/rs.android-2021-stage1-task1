dd58cdd08f381cd66d6865dff7f72106cff6f4dc
sergey_ivanenko <storm06rk@gmail.com>
2021-05-23 08:10:51 +0300

    init: start subtask3

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,7 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
+        
         throw NotImplementedError("Not implemented")
     }
 }
