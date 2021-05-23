a129439d18037d3b5f05583cec0754fcd0c8217a
sergey_ivanenko <storm06rk@gmail.com>
2021-05-23 07:57:11 +0300

    feat: subtask2 has been done

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,7 +5,19 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
+        val annaBill: Int;
+        var commonBill = 0;
 
-        throw NotImplementedError("Not implemented")
+        for (i in bill.indices) {
+            commonBill += bill[i];
+        }
+
+        annaBill = (commonBill - bill[k]) / 2;
+
+        return if (annaBill == b) {
+            "Bon Appetit";
+        } else {
+            (b - annaBill).toString();
+        }
     }
 }
