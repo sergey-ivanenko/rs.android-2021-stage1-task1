78c565cc64a0c118502dbae9360dedcbaecba530
sergey_ivanenko <storm06rk@gmail.com>
2021-05-31 20:14:38 +0300

    feat: subtask3 has been done

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,7 +4,32 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        
-        throw NotImplementedError("Not implemented")
+        val arrString = ArrayList<String>()
+        var count = 0
+
+        inputString.forEachIndexed { index, element ->
+            if (element == '[' || element == '<' || element == '(') {
+                val end = when (element) {
+                    '<' -> '>'
+                    '[' -> ']'
+                    '(' -> ')'
+                    else -> ' '
+                }
+                for (i in index + 1 until inputString.length) {
+                    if (inputString[i] == element) {
+                        count++
+                    }
+                    if (inputString[i] == end) {
+                        if (count == 0) {
+                            arrString.add(inputString.substring(index + 1 until i))
+                        } else {
+                            count--
+                        }
+                    }
+                }
+            }
+        }
+
+        return arrString.toTypedArray()
     }
 }
